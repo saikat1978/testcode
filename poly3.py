0515b221e65e6197cdf7cd29fc833dc7a8bd7620
Saikat Chatterjee <Saikat.Chatterjee@wdc.com>
2021-04-20 21:12:26 +0530

    code to generate all three IRIS defect mapping images

--- poly3.py
+++ poly3.py
@@ -9,45 +9,51 @@ matplotlib.use("TkAgg")
 import seaborn as sns
 
 
-csvFile = sys.argv[1]
+typeMap = {
+    1: 'Hook-WaterStain', 2: 'Polygon', 3: 'SymmetryMoon'
+}
 
-df = pd.read_csv(csvFile, header=[0, 1])
-#print(len(df.columns))
-df.dropna(how='all', axis=1, inplace=True)
-#print(len(df.columns))
+def generateImage(typeInfo: int, inputFolderLoc: str):
+    outputFolder = os.path.join(os.getcwd(), typeMap[typeInfo])
 
-dataSize = 0
-largestDataIndex = 0
-largestDf = Any
+    if os.path.exists(outputFolder) and os.path.isdir(outputFolder):
+        os.removedirs(outputFolder)
+    
+    os.mkdir(outputFolder)
+    
+    csvFolderLoc = os.path.join(inputFolderLoc, 'CSV')
+    files = os.listdir(csvFolderLoc)
+    
+    for csvFile in files:
+        print(f'processing {csvFile}')
+        df = pd.read_csv(os.path.join(csvFolderLoc, csvFile), header=[0, 1])
+        df.dropna(how='all', axis=1, inplace=True)
+        dataSize = 0
+        largestDataIndex = 0
+        largestDf = Any
 
-for i in range(0, len(df.columns), 2):
-    dataList = df.iloc[:, [i, i+1]]
-    dataList = dataList.dropna()
-    dataList = dataList.reset_index(drop=True)
-    #print(f'index = {i}')
-    #print(dataList)
-    
-    size = len(dataList.index)
-    if size > dataSize:
-        dataSize = size
-        largestDataIndex = i
-        largestDf = dataList
-        print(f'data size = {dataSize}, largest index = {largestDataIndex}')
-    
-    
+        for i in range(0, len(df.columns), 2):
+            dataList = df.iloc[:, [i, i+1]]
+            dataList = dataList.dropna()
+            dataList = dataList.reset_index(drop=True)
+            #print(f'index = {i}')
+            #print(dataList)
+            
+            size = len(dataList.index)
+            if size > dataSize:
+                dataSize = size
+                largestDataIndex = i
+                largestDf = dataList
+                print(f'data size = {dataSize}, largest index = {largestDataIndex}')
+            
+        plt.scatter(largestDf[largestDf.columns[0]].tolist(), 
+            largestDf[largestDf.columns[1]].tolist(), s=2, alpha=0.3, cmap='viridis')  
+        plt.savefig(os.path.join(outputFolder, os.path.splitext(csvFile)[0] + '.png'), dpi=300)  
         
 
-#print(dataSize, largestDataIndex)
-#print(largestDf)
 
-plt.scatter(largestDf[largestDf.columns[0]].tolist(), largestDf[largestDf.columns[1]].tolist(), s=2)
-plt.show()
 
-'''
-sns.set_theme(style="whitegrid")
-sns.color_palette("tab10")
-f, ax = plt.subplots(figsize=(6.5, 6.5))
-sns.despine(f, left=True, bottom=True)
-sns.scatterplot(x=largestDf.columns[0], y=largestDf.columns[1], data=largestDf)
-plt.show()
-'''
+if __name__=='__main__':
+    typeInfo = int(sys.argv[1])
+    folderPath = sys.argv[2]
+    generateImage(typeInfo, folderPath)
